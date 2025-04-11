27840064cbd71f187f35fd808c4cfc7aef15f9e3
Ali Yaman <aliymn.db@gmail.com>
2025-04-11 20:42:17 +0200

    feat: updated

--- libs/models/__init__.py
+++ libs/models/__init__.py
@@ -3,7 +3,7 @@ from libs.models.user import User
 from libs.models.body_tracker import BodyTracker
 from libs.models.daily_tracker import DailyTracker
 from libs.models.notifications import Notification
-from libs.models.fasting import FastingPlan, FastingSession
+from libs.models.fasting import FastingPlan
 from libs.models.blog import Blog, BlogCategory, BlogTag
 
 __all__ = [
@@ -13,7 +13,6 @@ __all__ = [
     "DailyTracker",
     "Notification",
     "FastingPlan",
-    "FastingSession",
     "Blog",
     "BlogCategory",
     "BlogTag",
