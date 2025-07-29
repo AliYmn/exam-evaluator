7cd9a9b6df71c29852c0792cfe59395ede444417
AliYmn <aliymn.db@gmail.com>
2025-07-29 19:16:49 +0000

    refactor: improve code structure

--- libs/migrations/env.py
+++ libs/migrations/env.py
@@ -136,3 +136,4 @@ else:
 # Updated on 2025-07-26: Improved code documentation
 # Refactored on 2025-07-26: Improved code structure
 # Updated on 2025-07-29: Improved code documentation
+# Refactored on 2025-07-29: Improved code structure
